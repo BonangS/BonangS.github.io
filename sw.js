c4cd3b72673e4d00cbd8aba948eeead8c41774f5
BonangS <ansellbonang4@gmail.com>
2025-03-24 08:31:18 +0700

    perubahan 3

--- sw.js
+++ sw.js
@@ -17,22 +17,44 @@ self.addEventListener('install', function(event) {
     return self.clients.claim();
   });
 
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(
+          keyList.map(function(key) {
+            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    return self.clients.claim();
+  });
+  
 
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      fetch(event.request)
-        .then(function(res) {
-          return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function(cache) {
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request) 
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, res.clone());
+                  return res;
+                });
+            });
         })
-        .catch(function(err) {
-          return caches.match(event.request);
+        .catch(function() {
+          return caches.match('/index.html'); 
         })
     );
   });
+  
 
 
 
